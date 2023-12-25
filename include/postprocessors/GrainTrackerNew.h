b8646b1cae6829884e6a0048fb2fce8093c3186e
PengWei97 <1248110286@qq.com>
2023-12-25 11:15:50 +0800

    create GrainTrackerNew considering num of adjacent grains

---
+++ include/postprocessors/GrainTrackerNew.h
@@ -0,0 +1,21 @@
+// Added function: When misorientation is below a certain threshold, grains merge
+
+#pragma once
+
+#include "GrainTracker.h"
+#include "MisorientationAngleCalculator.h"
+
+class GrainTrackerNew : public GrainTracker
+{
+public:
+  static InputParameters validParams();
+
+  GrainTrackerNew(const InputParameters & parameters);
+
+protected:
+  // establish the vector of adjacent grains based on the topological relationship
+  virtual void createAdjacentIDVector() override;
+
+  // This data structure is used to store the mapping from Grain ID to index in feature sets
+  std::map<unsigned int, std::size_t> _feature_id_to_index_maps;
+};
